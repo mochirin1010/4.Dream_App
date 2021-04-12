2184938521954577b3acfd5864670e9f4a9adbb4
yamajimomoyo <mochirin0530@gmail.com>
2021-04-12 19:54:41 +0900

    edit app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const mysql = require('mysql');
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const app = express();
-const PORT = process.env.PORT || 3000;
-listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// server.listen(process.env.PORT || 3000);
 
 app.set("view engine", "ejs");
 
