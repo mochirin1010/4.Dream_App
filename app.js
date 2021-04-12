b317767f4e860d5f6ccd126c659288f560a9a6f5
yamajimomoyo <mochirin0530@gmail.com>
2021-04-12 20:15:25 +0900

    app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.listen(process.env.PORT || 3000);
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
